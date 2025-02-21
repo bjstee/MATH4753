b456bc8dab23f26979c5fa14c1bedb708c5c0a83
bjstee <111779778+bjstee@users.noreply.github.com>
2025-02-21 15:07:34 -0600

    proper commit

--- R/mysq.R
+++ R/mysq.R
@@ -1,11 +1,12 @@
-#' Mysquared function
+#' @title Mysquared Function
 #'
-#' @param x A quantitative vector
+#' @param x number to be squared (as a vector)
 #'
-#' @returns A quantitative vector with squared components
+#' @returns the square of x (as a vector)
 #' @export
 #'
 #' @examples
+#' mysq(4)
 #' mysq(1:10)
 mysq <- function(x) {
   x^2
